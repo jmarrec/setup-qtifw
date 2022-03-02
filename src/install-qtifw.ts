2471b5cea874e55928cb84e66302872c3e4069b8
Julien Marrec <julien.marrec@gmail.com>
2022-03-02 19:04:35 +0100

    :'( another one.

--- src/install-qtifw.ts
+++ src/install-qtifw.ts
@@ -194,7 +194,7 @@ export async function installRequiredSystemDeps() {
       await exec.exec('sudo', ['apt-get', 'update'], {silent: true});
 
       core.info(
-        'Installing required system libraries: libxkbcommon-x11-0 xorg-dev libgl1-mesa-dev libxcb-icccm4-dev libxcb-image0-dev libxcb-keysyms1-dev libxcb-render-util0-dev'
+        'Installing required system libraries: libxkbcommon-x11-0 xorg-dev libgl1-mesa-dev libxcb-icccm4-dev libxcb-image0-dev libxcb-keysyms1-dev libxcb-render-util0-dev libxcb-xinerama-dev'
       );
 
       await exec.exec(
@@ -209,7 +209,8 @@ export async function installRequiredSystemDeps() {
           'libxcb-icccm4-dev',
           'libxcb-image0-dev',
           'libxcb-keysyms1-dev',
-          'libxcb-render-util0-dev'
+          'libxcb-render-util0-dev',
+          'libxcb-xinerama-dev'
         ],
         {silent: true}
       );
